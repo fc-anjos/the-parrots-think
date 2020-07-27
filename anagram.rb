b2d6e1226f31657cae5c1489c94de714e38755a6
MiguelDP4 <migueldp4@gmail.com>
2020-07-27 08:31:22 -0600

    pseudo code for anagram algorithm

---
+++ anagram.rb
@@ -0,0 +1,30 @@
+# 1. Divide string in 2
+# 2. read each letter on both strings
+# 3. remove each matching letter
+# 4. Leave only letters with no match
+# 5. Count matching letters
+
+# asdf | awdf
+# asdf
+# awdf
+
+# felipe
+# felope
+
+# elipe
+# elope
+
+# lipe
+# lope
+
+# ipe
+# ope
+
+# ie
+# oe
+
+# i
+# o
+
+# ne
+# ui

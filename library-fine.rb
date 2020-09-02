f2a46d5ad62333ec33adfb638d62a12377a92c7d
MiguelDP4 <migueldp4@gmail.com>
2020-09-02 08:11:40 -0600

    fix library fine

--- library-fine.rb
+++ library-fine.rb
@@ -1,17 +1,25 @@
+require 'json'
+require 'date'
+require 'stringio'
+
+# Complete the libraryFine function below.
 def libraryFine(d1, m1, y1, d2, m2, y2)
   date1 = DateTime.new(y1,m1,d1)
   date2 = DateTime.new(y2,m2,d2)
   
   dif = (date1 - date2).to_i
-  puts dif
   if dif <= 0
       fine = 0
   elsif dif > 0 && m2 == m1 && y2 == y1
       fine = 15 * dif
-  elsif m2 > m1 && y2 == y1
-      fine = 500 * m2 - m1
-  elsif y2 > y1
+  elsif m2 < m1 && y2 == y1
+      fine = 500 * (m1 - m2)
+  elsif y2 < y1
      fine = 10000 
   end
-  fine
+    puts fine
+  return fine
+
 end
+
+puts libraryFine(24, 2, 2015, 15, 2, 2015)
